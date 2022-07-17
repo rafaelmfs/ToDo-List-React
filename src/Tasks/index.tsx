7a56ef6012c12ca2ba67d995262da0251c5aaab8
Rafael Marques <rafael.marques-98@hotmail.com>
2022-07-17 08:03:35 -0300

    Criando e estilizando o componente TaskList e Task

--- src/Tasks/index.tsx
+++ src/Tasks/index.tsx
@@ -1,8 +1,35 @@
+import { Clipboard } from './Clipboard'
+import { Task } from './Task'
+import styles from './Tasks.module.css'
+
 export function Tasks(){
   return (
-    <section>
-      <header></header>
-      <div></div>
+    <section className={styles.tasks}>
+      <header>
+        <div className={styles.createdTasks}>
+          <strong>Tarefas criadas</strong>
+          <span>0</span>
+        </div>
+        <div className={styles.completedTasks}>
+          <strong>Concluídas</strong>
+          <span>0</span>
+        </div>
+      </header>
+
+      <div className={styles.tasksEmpty}>
+        <Clipboard />
+        <div className={styles.texts}>
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          <span>Crie tarefas e organize seus itens a fazer</span>
+        </div>
+      </div>
+
+      {/* <ul className={styles.tasksList}>
+        <li>
+          <Task />
+        </li>
+      </ul> */}
+
     </section>
   )
 }
